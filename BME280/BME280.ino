10b3488d938a8900a51707211589f0303ef0fcd0
JHE060297 <jhoan.aristizabal@uniminuto.edu.co>
2023-06-15 00:00:25 -0500

    Se añade sensor DHT22

--- BME280/BME280.ino
+++ BME280/BME280.ino
@@ -1,37 +1,41 @@
+#include <DHT.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
+#define DHT_PIN 2
+#define DHT_TYPE DHT22
+
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+DHT dht(DHT_PIN, DHT_TYPE);
 Adafruit_BME280 bme;
 
-void setup() {
-	Serial.begin(9600);
 
-	if (!bme.begin(0x76)) {
-		Serial.println("No se ha podido encontrar un sensor BME280, compruebe el cableado!");
-		while (1);
-	}
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+
+  if (!bme.begin(0x76)) {
+    Serial.println("No se ha podido encontrar un sensor BME280, compruebe el cableado!");
+    while (1)
+      ;
+  }
 }
 
 void loop() {
-	Serial.print("Temperatura = ");
-	Serial.print(bme.readTemperature());
-	Serial.println("*C");
+  Serial.print("TemperaturaBME= ");
+  Serial.print(bme.readTemperature());
+  Serial.print(", Presion= ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.print(", Altitud= ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.print(", HumedadBME= ");
+  Serial.print(bme.readHumidity());
+  Serial.print(", TemperaturaDHT= ");
+  Serial.print(dht.readTemperature());
+  Serial.print(", HumedadDHT= ");
+  Serial.println(dht.readHumidity());
 
-	Serial.print("Presion = ");
-	Serial.print(bme.readPressure() / 100.0F);
-	Serial.println("hPa");
-
-	Serial.print("Aprox. Altitud = ");
-	Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-	Serial.println("m");
-
-	Serial.print("Humedad = ");
-	Serial.print(bme.readHumidity());
-	Serial.println("%");
-
-	Serial.println();
-	delay(1000);
+  delay(1000);
 }
