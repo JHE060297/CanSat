874e7a40f0cc71d35085c9b2ddd098c57a62de44
JHE060297 <jhoan.aristizabal@uniminuto.edu.co>
2023-06-15 19:07:13 -0500

    DB Successful

--- cansat.py
+++ cansat.py
@@ -32,15 +32,12 @@ try:
     # Imprimir un mensaje si la conexión se estableció correctamente
     print("Conexión exitosa a la base de datos.")
 
-    # Resto del código...
-
 except pyodbc.Error as ex:
     # Imprimir un mensaje si se produce un error al establecer la conexión
     print("Error al establecer la conexión a la base de datos:", ex)
 
-
 tiempo_inicio = time.time()
-# Leer los datos serie y escribirlos en el archivo CSV
+
 while True:
     # Leer una línea de datos del Arduino
     data = ser.readline().decode().strip()  # Decodificar los bytes recibidos a texto
@@ -57,17 +54,16 @@ while True:
             numero = float(matches[0])  # Convertir a tipo float
             numeros.append(numero)
 
-    valores_str = ", ".join(str(numero) for numero in numeros)
+    consulta = f"INSERT INTO DatosSensor (tempBME, presion, altitud, humBME, tempDHT, humDHT) VALUES (?,?,?,?,?,?)"
 
-    consulta = f"INSERT INTO DatosSensor (tempBME, presion, altitud, humBME, tempDHT, humDHT) VALUES ({valores_str}) "
-
-    cursor.execute(consulta)
-    conn.commit()
+    for valor in numeros:
+        cursor.execute(consulta, numeros)
+        conn.commit()
 
     # Detener la captura después de un minuto
     tiempo_actual = time.time()
     tiempo_transcurrido = tiempo_actual - tiempo_inicio
-    if tiempo_transcurrido >= 18000:
+    if tiempo_transcurrido >= 10800:
         break
 
 # Cerrar conexion BD y la comunicación serie
